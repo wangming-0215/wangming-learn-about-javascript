2f159c2838060f2d8e9758120687cb106d7586a6
xiaoming <xiaomingboy0215.163.com>
2017-07-30 00:49:47 +0800

    recoding"

--- webpack/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.common');
-const devConfig = require('./webpack.dev');
-
-module.exports = (env) => {
-    if (env === 'development') {
-        return webpackMerge(commonConfig, devConfig);
-    }
-};
